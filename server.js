a837568b05495bd6518ccf2689bba507af6ca241
sibsankarm123 <94391047+sibsankarm123@users.noreply.github.com>
2026-06-18 06:50:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const rateLimit = require('express-rate-limit');
 require('dotenv').config();
 const Razorpay = require('razorpay');
 const crypto   = require('crypto');
-
+a
 const razorpay = new Razorpay({
   key_id:     process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET
